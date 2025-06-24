4d461de819470a1ca167ff800b86e7de5861b809
YeonJ-K <59119195+YeonJ-K@users.noreply.github.com>
2025-06-24 11:24:37 +0900

    Create Monster.h

---
+++ textRPG/Monster/Monster.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+//몬스터 인터페이스	정의
+class Monster
+{
+public:
+	virtual std::string getName() const = 0;
+	virtual void takeDamage(int damage) = 0;
+	virtual int getHealth() const = 0;
+	virtual int getAttack() const = 0;
+	virtual int getExpReward() const = 0;
+	virtual int getGoldReward() const = 0;
+	//virtual Item* dropItem() const = 0;
+
+	virtual ~Monster() = default;
+};
